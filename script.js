c62419619a62e6ba41d00c246713685d15741126
Danil Murzin <danil.murzin.200026@gmail.com>
2024-04-12 13:43:39 +0300

    add lesson02

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = 'glo';
+let screens = 'Simple, Complex, Interactive';
+let screenPrice = 400;
+let rollback = 30;
+let fullPrice = 70000;
+let adaptive = false;
 
-console.log('Hi!');
-alert('Hello!');
+console.log('title: ', typeof title);
+console.log('fullPrice: ', typeof fullPrice);
+console.log('adaptive: ', typeof adaptive);
+
+console.log('screens.length', screens.length);
+console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
+console.log(`Стоимость разработки сайта ${fullPrice} рублей`);
+
+console.log(screens.toLowerCase().split(', '));
+
+console.log('Percent: ', fullPrice * (rollback / 100));
